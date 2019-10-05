e5a9685d463cd9c553af7eca2712e710b395bd1e
AceIsHuman <menoman1995@yahoo.com>
2019-10-05 13:18:07 -0500

    build friends list component

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { Card, Icon, Header, Container } from "semantic-ui-react";
+
+const FriendsList = props => {
+  const [friends, setFriends] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("friends")
+      .then(res => {
+        setFriends(res.data);
+      })
+      .catch(err => {
+        console.error(err);
+        alert("Unable to get friends list");
+      });
+  }, []);
+
+  return (
+    <Container>
+      <Header>Your Friends</Header>
+      <Card.Group centered>
+        {friends.map(friend => (
+          <Card key={friend.id}>
+            <Card.Content>
+              <Card.Header>
+                <Icon name="user" color="blue" />
+                {friend.name}
+              </Card.Header>
+              <Card.Meta>Age: {friend.age}</Card.Meta>
+              <Card.Description>
+                <Icon name="mail" color="green" />
+                {friend.email}
+              </Card.Description>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Container>
+  );
+};
+
+export default FriendsList;
