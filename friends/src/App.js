c3f4282d946ac941b5387acdc5be3dbeabbdfcac
AceIsHuman <menoman1995@yahoo.com>
2019-10-05 11:58:54 -0500

    set credentials to state

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
+import Login from './components/Login';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Login />
+    </>
   );
 }
 
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Button, Form, Header } from "semantic-ui-react";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value });
+  };
+
   return (
-    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
       <Grid.Column style={{ maxWidth: 450 }}>
         <Header as="h2">Please Login</Header>
         <Form size="large">
-          <Form.Input icon="user" placeholder="Username" fluid />
-          <Form.Input icon="lock" placeholder="Password" fluid />
+          <Form.Input icon="user" placeholder="Username" fluid name='username' value={credentials.username} onChange={handleChange} />
+          <Form.Input icon="lock" placeholder="Password" fluid name='password' value={credentials.password} onChange={handleChange} />
           <Button color="facebook" fluid>
             Submit
           </Button>
