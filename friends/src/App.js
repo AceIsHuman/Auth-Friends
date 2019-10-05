0c20833544b36242a271a7a78650d9a59479ee8a
AceIsHuman <menoman1995@yahoo.com>
2019-10-05 12:04:15 -0500

    build route for login page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css';
 
@@ -6,9 +7,12 @@ import Login from './components/Login';
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Router>
+      <Link to='/login'>Login Here!</Link>
+      <Switch>
+        <Route path='/login' component={Login} />
+      </Switch>
+    </Router>
   );
 }
 
