5bfb19e235b25a3b716adbc820b6cc9a605a49c7
AceIsHuman <menoman1995@yahoo.com>
2019-10-05 13:02:51 -0500

    create PrivateRoute for friends list

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
-import './App.css';
-import 'semantic-ui-css/semantic.min.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
 
-import Login from './components/Login';
+import PrivateRoute from "./components/PrivateRoute";
+import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
 
 function App() {
   return (
     <Router>
-      <Link to='/login'>Login Here!</Link>
+      <Link to="/login">Login Here!</Link>
       <Switch>
-        <Route path='/login' component={Login} />
+        <PrivateRoute exact path="/friends" component={FriendsList} />
+        <Route path="/login" component={Login} />
       </Switch>
     </Router>
   );
